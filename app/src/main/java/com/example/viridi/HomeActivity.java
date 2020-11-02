92309c8d9875e7c6519660a3d391e7f7b04f1e7e
Chander Shekhar <be.like.chandu@gmail.com>
2020-11-02 20:24:20 +0530

    navigation drawer added

--- app/src/main/java/com/example/viridi/HomeActivity.java
+++ app/src/main/java/com/example/viridi/HomeActivity.java
@@ -1,16 +1,75 @@
 package com.example.viridi;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
 
+        drawer = findViewById(R.id.drawer_activity);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.book:
+                Intent intBook = new Intent(HomeActivity.this, BookActivity.class);
+                startActivity(intBook);
+                break;
+            case R.id.buy:
+                Intent intBuy = new Intent(HomeActivity.this, BuyActivity.class);
+                startActivity(intBuy);
+                break;
+            case R.id.sell:
+                Intent intSell = new Intent(HomeActivity.this, SellActivity.class);
+                startActivity(intSell);
+                break;
+            case R.id.learn:
+                Intent intLearn = new Intent(HomeActivity.this, LearnActivity.class);
+                startActivity(intLearn);
+                break;
+            case R.id.craftWork:
+                Intent intent1 = new Intent(HomeActivity.this, CraftMarketActivity.class);
+                startActivity(intent1);
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
