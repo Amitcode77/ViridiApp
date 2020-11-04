a03593453694b08d7dfc08f13a3c1eef0c580674
amitcode77 <amitcode77@gmail.com>
2020-11-04 12:16:40 +0530

    Learn Activity Added

--- app/src/main/java/com/example/viridi/LearnActivity.java
+++ app/src/main/java/com/example/viridi/LearnActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
 public class LearnActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
+
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view1);
+        getLifecycle().addObserver(youTubePlayerView);
+
     }
 }
