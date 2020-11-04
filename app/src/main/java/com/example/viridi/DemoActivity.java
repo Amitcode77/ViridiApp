5025853547c86f49e0758c76c810cd1938214328
amitcode77 <amitcode77@gmail.com>
2020-11-04 08:26:55 +0530

    Added project zip file

---
+++ app/src/main/java/com/example/viridi/DemoActivity.java
@@ -0,0 +1,16 @@
+package com.example.viridi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+
+    }
+}
